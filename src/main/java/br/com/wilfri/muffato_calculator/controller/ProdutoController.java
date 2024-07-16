4227fd66a67d131fce5c7dac2abdc2e79dc8183a
enzojpn <enzo_jpn@hotmail.com>
2024-07-15 21:43:37 -0300

    Criando Produto Controller

---
+++ src/main/java/br/com/wilfri/muffato_calculator/controller/ProdutoController.java
@@ -0,0 +1,16 @@
+package br.com.wilfri.muffato_calculator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ProdutoController { 
+	
+	@GetMapping("produtos")
+	public String lista() {
+		return "é para enviar a lista dos produtos...exemplo {cafe , coca cola e picanha}" ;  
+	}
+	 
+	
+}
---
+++ src/test/java/br/com/wilfri/muffato_calculator/MuffatoCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wilfri.muffato_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuffatoCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
