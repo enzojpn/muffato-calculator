44449f3fbfe3b8e747dc5e7d21ee3191e530b0cb
CaaPinheiro <carlinhosgomes1513@gmail.com>
2024-07-15 22:19:09 -0300

    Criando health-check

--- src/main/java/br/com/wilfri/muffato_calculator/controller/HealthCheckController.java
+++ src/main/java/br/com/wilfri/muffato_calculator/controller/HealthCheckController.java
@@ -3,14 +3,12 @@ package br.com.wilfri.muffato_calculator.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-public class ProdutoController { 
-	
-	@GetMapping("produtos")
-	public String lista() {
-		return "é para enviar a lista dos produtos...exemplo { coca cola e picanha}" ;  
+public class HealthCheckController {
+
+	@GetMapping("health-check")
+	public String alert() {
+		return "true";
 	}
-	 
-	
+
 }
