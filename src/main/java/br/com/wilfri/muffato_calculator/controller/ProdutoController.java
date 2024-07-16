4f7091ddbe618d6c027f359aa2dc8f0c4a033173
enzojpn <enzo_jpn@hotmail.com>
2024-07-15 22:28:50 -0300

    batata frita

--- src/main/java/br/com/wilfri/muffato_calculator/controller/ProdutoController.java
+++ src/main/java/br/com/wilfri/muffato_calculator/controller/ProdutoController.java
@@ -3,15 +3,13 @@ package br.com.wilfri.muffato_calculator.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-public class ProdutoController { 
-	
+public class ProdutoController {
+
 	@GetMapping("produtos")
 	public String lista() {
-		System.out.println("log no terminal"); //TODO: apagar
-		return "é para enviar a lista dos produtos...exemplo { coca cola e picanha}" ;  
+		System.out.println("log no terminal"); // TODO: apagar
+		return "é para enviar a lista dos produtos...exemplo { batafrita ...coca cola e picanha}";
 	}
-	 
-	
+
 }
