ffa69f1ae00dd05f5e49ef5e15891e01d8c78899
CaaPinheiro <carlinhosgomes1513@gmail.com>
2024-07-16 20:19:11 -0300

    Revert "url health criada. close #1"
    This reverts commit 506388f79bdd9263392db29d1761d2876413fe86.

--- src/main/java/br/com/wilfri/muffato_calculator/controller/HealthCheckController.java
+++ src/main/java/br/com/wilfri/muffato_calculator/controller/HealthCheckController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-	@GetMapping("health")
+	@GetMapping("health-check")
 	public String alert() {
-		return "UP";
+		return "true";
 	}
 
 }
